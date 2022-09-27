e7190862f76f9036618918f522816c41e051443f
Utkarsh0008 <108312829+Utkarsh0008@users.noreply.github.com>
2022-09-27 14:32:41 +0530

    Update Tower_of_Hanoi.java

--- Tower_of_Hanoi.java
+++ Tower_of_Hanoi.java
@@ -1,6 +1,7 @@
 package Recursion;
 
 import java.util.*;
+
 public class Tower_of_Hanoi {
 public static int count=0;
 	public static void main(String[] args) {
@@ -14,15 +15,15 @@ public static int count=0;
         System.out.println(count);
 
 	}
-
+      //SARA GAME BS PARAMETERS BDLKR REC. CALL BHJNE KA HAI
 	private static void TOH(int n, String src, String help, String dest) {
 		if(n==0) {
-			return;
+			return;  //base case ki n=0 to return
 		}
-		TOH(n-1, src, dest, help);
-		System.out.println("move "+n+"th disc from "+src+" to "+dest);
+		TOH(n-1, src, dest, help); //n-1 disc ko src to help mei dalwa rhe hai from recursion
+		System.out.println("move "+n+"th disc from "+src+" to "+dest);//self work i.e jo bcha disk hai usko src to dest. 
 		count++;
-		TOH(n-1, help,src , dest);
+		TOH(n-1, help,src , dest);//jo n-1 wale disc the unhe help se dest mei daal dege using rec.
 		
 	}
 
